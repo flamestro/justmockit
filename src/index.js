2fe700aca3c332f448e70ad161179cd2ed59eb81
flamestro <NoReplyFlamestro@gmail.com>
2021-07-22 23:05:04 +0200

    JMI #4: add mock structure and setup business logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const app = express()
-
+const mocks = require('./mocks.json')
+const util = require("util");
 let bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded());
 
+
 app.use((req, res) => {
+    // check if response should be mocked
+
+    // check query parameters
+    for (let mock of mocks) {
+        if (req.method.toUpperCase() === mock.method.toUpperCase() && req.path === mock.path) {
+            if (mock.query && req.query && util.isDeepStrictEqual(req.query, mock.query)) {
+                res.status(mock.res.status).header({"content-type": "application/json"}).send(mock.res.body)
+                return;
+            }
+        }
+    }
+
+    // check without parameters TODO: simplify duplicated code
+    for (let mock of mocks) {
+        if (req.method.toUpperCase() === mock.method.toUpperCase() && req.path === mock.path) {
+                res.status(mock.res.status).header({"content-type": "application/json"}).send(mock.res.body)
+                return
+        }
+    }
+
+    // else echo
     res.status(200).header({"content-type": "application/json", "method": req.method}).send(req.body)
 })
 
