04e677296020443cfee8653eb13d94cec7dca28a
flamestro <NoReplyFlamestro@gmail.com>
2021-07-24 13:13:41 +0200

    JMI #5: optimization

--- src/index.js
+++ src/index.js
@@ -10,23 +10,33 @@ app.use(bodyParser.urlencoded());
 
 
 app.use((req, res) => {
-    // check if response should be mocked
-
-    // check query parameters
-    for (let mock of mocks) {
-        if (req.method.toUpperCase() === mock.method.toUpperCase() && req.path === mock.path) {
-            if (mock.query && req.query && util.isDeepStrictEqual(req.query, mock.query)) {
-                res.status(mock.res.status).header({"content-type": "application/json"}).send(mock.res.body)
-                return;
-            }
-        }
+    const determinePriorityOfMock = (mock) => {
+        if(mock.req.query) return 0;
+        return 1;
     }
 
-    // check without parameters TODO: simplify duplicated code
-    for (let mock of mocks) {
-        if (req.method.toUpperCase() === mock.method.toUpperCase() && req.path === mock.path) {
-                res.status(mock.res.status).header({"content-type": "application/json"}).send(mock.res.body)
-                return
+    const mocksSortedByPriority = mocks.sort((mockA, mockB) => {
+        if(determinePriorityOfMock(mockA) < determinePriorityOfMock(mockB)){
+            return -1;
+        }
+        if(determinePriorityOfMock(mockA) > determinePriorityOfMock(mockB)){
+            return 1;
+        }
+        return 0;
+    })
+
+    const checkIfMockFitsRequest = (request, mock) => {
+        return request.method.toUpperCase() === mock.req.method.toUpperCase()
+            && request.path === mock.req.path
+            && (util.isDeepStrictEqual(req.query, mock.req.query) || !mock.req.query && !req.query.keys);
+    }
+
+    for (let mock of mocksSortedByPriority) {
+        if (checkIfMockFitsRequest(req, mock)) {
+                res.status(mock.res.status)
+                    .header({"content-type": "application/json"})
+                    .send(mock.res.body)
+                return;
         }
     }
 
